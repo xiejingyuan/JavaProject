a58875247a465b698290377d5b14c6ee6ac282aa
xiejingyuan <110453023+xiejingyuan@users.noreply.github.com>
2024-04-08 09:34:15 -0400

    Update Dictionary.java

--- src/Dictionary.java
+++ src/Dictionary.java
@@ -3,9 +3,9 @@ public class Dictionary {
 
     private int category;
     private final String[][] dic = {
-            {"dog", "cat", "rabbit", "parrot", "snake", "fish"},
-            {"apple", "banana", "orange", "grape", "blueberry", "watermelon"},
-            {"football", "swimming", "basketball", "running", "hiking", "tennis"}
+        {"TORONTO","MONTREAL","CALGARY","VANCOUVER","OTTAWA","EDMONTON","QUEBEC","WINNIPEG","HALIFAX","VICTORIA"},
+        {"APPLE", "BANANA", "ORANGE", "GRAPE", "BLUEBERRY", "WATERMELON", "PINEAPPLE", "STRAWBERRY", "MANGO", "KIWI"},
+        {"SOCCER","TENNIS","GOLF","SWIMMING","HOCKEY","BASEBALL","BADMINTON","CURLING","SKIING","RUGBY"}
     };
 
     public String selectWord () {
@@ -18,7 +18,7 @@ public class Dictionary {
     public String hint () {
         switch (category) {
             case 0:
-                return "animals";
+                return "city in Canada";
             case 1:
                 return "fruit";
             case 2:
